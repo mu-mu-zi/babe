4b27f0f5e3d6eb0f8fa4db1f1d339bdb9d4dada4
木木子 <mumu@163.com>
2020-07-03 20:35:02 +0800

    js第三次

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var div = document.getElementsByTagName('div')
 
-div.click = ()=>{
+div[0].click = ()=>{
+    div.style.background = "green"
+}
+
+div[1].click = ()=>{
     div.style.background = "green"
 }
